647b705840d942ffd004bd4cb05060251a5b7b71
aemirdnr <aemirdnr@gmail.com>
2022-10-07 03:15:49 +0300

    Design: User Menu

--- src/components/Navbar/User.jsx
+++ src/components/Navbar/User.jsx
@@ -1,8 +1,28 @@
 import { KeyboardArrowDown } from "@mui/icons-material";
-import { Avatar, Box, IconButton, Typography, useTheme } from "@mui/material";
-import React from "react";
+import {
+  Avatar,
+  Box,
+  Divider,
+  IconButton,
+  Menu,
+  MenuItem,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
 
 function User() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+
+  const handleClose = (e) => {
+    setAnchorEl(null);
+  };
+
   return (
     <Box display="flex" gap="12px" marginRight="12px" alignItems="center">
       <Typography display={{ xs: "none", sm: "none", md: "block" }}>
@@ -14,10 +34,62 @@ function User() {
           src="/src/assets/avatar.jpg"
           sx={{ width: 36, height: 36 }}
         />
-        <IconButton>
-          <KeyboardArrowDown />
-        </IconButton>
+        <Tooltip title="Account settings">
+          <IconButton
+            onClick={handleClick}
+            aria-controls={open ? "account-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+          >
+            <KeyboardArrowDown />
+          </IconButton>
+        </Tooltip>
       </Box>
+      <Menu
+        id="account-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: "visible",
+            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+            mt: 1.5,
+            "& .MuiAvatar-root": {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            "&:before": {
+              content: '""',
+              display: "block",
+              position: "absolute",
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: "background.paper",
+              transform: "translateY(-50%) rotate(45deg)",
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+      >
+        <MenuItem> Profile</MenuItem>
+        <Divider />
+        <MenuItem> Settings</MenuItem>
+        <Divider />
+        <MenuItem>Extend the license</MenuItem>
+        <Divider />
+        <MenuItem>Support</MenuItem>
+        <Divider />
+        <MenuItem>Log out</MenuItem>
+      </Menu>
     </Box>
   );
 }
