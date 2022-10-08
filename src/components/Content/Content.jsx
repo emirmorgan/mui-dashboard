dd0d26e0d0e15094d6c330a426f50d9a1251f9e1
aemirdnr <aemirdnr@gmail.com>
2022-10-08 05:05:25 +0300

    Update: Remove useTheme

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,46 +1,27 @@
-import { Box, Container, useTheme } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import Cards from "./Cards";
 import Graphs from "./Graphs";
 import CVETable from "./CVETable";
 import User from "../Navbar/User";
 import Ticket from "../Navbar/Ticket";
 
-const useStyles = (theme) => ({
-  responsiveContainer: {
-    marginTop: "72px",
-    [theme.breakpoints.down("lg")]: {
-      maxWidth: "sm",
-    },
-    [theme.breakpoints.up("md")]: {
-      maxWidth: "xl",
-    },
-  },
-  responsiveBox: {
-    display: "none",
-    [theme.breakpoints.down("md")]: {
-      display: "flex",
-    },
-  },
-});
-
 function Content() {
-  const theme = useTheme();
-  const Classes = useStyles(theme);
-
   return (
-    <Container sx={Classes.responsiveContainer}>
-      <Box
-        display={Classes.responsiveBox}
-        justifyContent="space-between"
-        marginTop="32px"
-      >
-        <Ticket />
-        <User />
-      </Box>
-      <Cards />
-      <Graphs />
-      <CVETable />
-    </Container>
+    <Box marginTop="72px">
+      <Container>
+        <Box
+          display={{ xs: "flex", sm: "flex", md: "none" }}
+          justifyContent="space-between"
+          marginTop="32px"
+        >
+          <Ticket />
+          <User />
+        </Box>
+        <Cards />
+        <Graphs />
+        <CVETable />
+      </Container>
+    </Box>
   );
 }
 
